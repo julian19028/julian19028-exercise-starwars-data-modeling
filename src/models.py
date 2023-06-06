d3c2a107407e2c1cb60bae68ff8a3bfacffb5840
julian19028 <julian_ysv@Icloud.com>
2023-06-06 01:36:22 +0000

    pending

--- src/models.py
+++ src/models.py
@@ -7,15 +7,22 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class People(Base):
+    __tablename__ = 'People'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    Name = Column(String(250), nullable=False)
+    Height = Column(Integer, nullable=False)
+    Mass = Column(Integer, nullable=False)
+    Hair_Color = Column (String(250), nullable=False)
+    Skin_Color = Column (String(250), nullable=False)
+    Birth_Year = Column (String(250), nullable=True)
+    Gender = Column (String(250), nullable=False)
+    Homewolrd = Column (Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planets(Base):
+    __tablename__ = 'Planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
